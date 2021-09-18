2f5962c5f80ff70b0048cdb388c712a397e273f2
Abe Vriens <abe.vriens@gmail.com>
2021-09-18 19:28:38 +0200

    Initial commit

---
+++ src/main/java/com/abevriens/CockCityRaids.java
@@ -0,0 +1,16 @@
+package com.abevriens;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CockCityRaids extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable called.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable called.");
+    }
+
+}
