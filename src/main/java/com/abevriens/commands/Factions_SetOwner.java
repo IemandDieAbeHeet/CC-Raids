9dbdcca93c6a84fdd81232d695a9f852fdd27e7d
Abe Vriens <abe.vriens@gmail.com>
2021-10-13 23:28:00 +0200

    Change player index on owner change

--- src/main/java/com/abevriens/commands/Factions_SetOwner.java
+++ src/main/java/com/abevriens/commands/Factions_SetOwner.java
@@ -19,30 +19,42 @@ public class Factions_SetOwner extends Factions_Base {
     }
 
     private void command_SetOwner() {
-        CC_Player cc_kickplayer = CrackCityRaids.instance.playerManager.getCCPlayer(name);
+        CC_Player cc_ownerplayer = CrackCityRaids.instance.playerManager.getCCPlayer(name);
 
         if(cc_player.faction.factionName.equals(FactionManager.emptyFaction.factionName)) {
             ComponentBuilder errorMessage = TextUtil.GenerateErrorMsg("Je zit niet in een faction," +
                     " join er een met /factions join.");
             player.spigot().sendMessage(errorMessage.create());
         } else if(!cc_player.faction.factionOwner.uuid.equals(cc_player.uuid)) {
-            ComponentBuilder errorMessage = TextUtil.GenerateErrorMsg("Je bent niet de owner van deze faction, vraag aan" +
-                    cc_player.faction.factionOwner.displayName + " of ze " + name + " willen accepteren!");
+            ComponentBuilder errorMessage = TextUtil.GenerateErrorMsg("Je bent niet de owner van deze faction, vraag aan " +
+                    cc_player.faction.factionOwner.displayName + " of ze de faction owner willen veranderen!");
 
             player.spigot().sendMessage(errorMessage.create());
-        } else if(cc_kickplayer == null) {
+        } else if(cc_ownerplayer == null) {
             ComponentBuilder errorMessage = TextUtil.GenerateErrorMsg("De speler die je probeert owner te maken bestaat niet!");
 
             player.spigot().sendMessage(errorMessage.create());
-        } else if(!cc_kickplayer.faction.factionName.equals(cc_player.faction.factionName)) {
+        } else if(!cc_ownerplayer.faction.factionName.equals(cc_player.faction.factionName)) {
             ComponentBuilder errorMessage = TextUtil.GenerateErrorMsg("De speler die je probeert owner te maken zit niet in je faction! Bekijk alle leden met /factions info");
 
             player.spigot().sendMessage(errorMessage.create());
         } else {
-            POJO_Player pojo_playerowner = PlayerManager.CCToPOJO(cc_player);
+            int oldIndex = 0;
+            for(int i = 0; i < cc_ownerplayer.faction.players.size(); i++) {
+                if(cc_ownerplayer.faction.players.get(i).uuid.equals(cc_ownerplayer.uuid)) {
+                    oldIndex = i;
+                }
+            }
+            cc_ownerplayer.faction.players.set(0, cc_ownerplayer);
+            cc_ownerplayer.faction.players.set(oldIndex, cc_player);
+
+            POJO_Player pojo_playerowner = PlayerManager.CCToPOJO(cc_ownerplayer);
             cc_player.faction.factionOwner = pojo_playerowner;
+            OfflinePlayer offlinePlayerOwner = Bukkit.getOfflinePlayer(UUID.fromString(cc_ownerplayer.uuid));
             CrackCityRaids.instance.dbHandler.updateFaction(FactionManager.FactionToPOJO(cc_player.faction));
-            CrackCityRaids.instance.dbHandler.updatePlayer(pojo_playerowner);
+            ComponentBuilder successMsg = TextUtil.GenerateSuccessMsg( pojo_playerowner.displayName + " is de owner van " +
+                    "de faction geworden!");
+            cc_player.faction.sendMessageToPlayers(successMsg);
         }
     }
 }
