645e276ee34e2534841958d929485631e2009821
Abe Vriens <abe.vriens@gmail.com>
2021-10-17 13:11:39 +0200

    Add error checking

--- src/main/java/com/abevriens/BlockClickEventListener.java
+++ src/main/java/com/abevriens/BlockClickEventListener.java
@@ -1,6 +1,7 @@
 package com.abevriens;
 
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -18,7 +19,17 @@ public class BlockClickEventListener implements Listener {
             Block block = event.getClickedBlock();
             if(block.getType() == Material.STRUCTURE_BLOCK) {
                 FactionBlock factionBlock = CrackCityRaids.instance.factionBlockManager.getFactionBlock(block);
-                if(!cc_player.faction.factionName.equals(factionBlock.factionName)) {
+                if(factionBlock == null) {
+                    ComponentBuilder errorMsg = TextUtil.GenerateErrorMsg("Kan het aangeklikte factionblok niet vinden, " +
+                            "waarschijnlijk is dit blok niet door een faction geplaatst");
+
+                    player.spigot().sendMessage(errorMsg.create());
+                } else if(!player.getGameMode().equals(GameMode.SURVIVAL)) {
+                    ComponentBuilder errorMsg = TextUtil.GenerateErrorMsg("Je moet in survival zitten als je het " +
+                            "faction blok wil gebruiken!");
+
+                    player.spigot().sendMessage(errorMsg.create());
+                } else if(!cc_player.faction.factionName.equals(factionBlock.factionName)) {
                     ComponentBuilder errorMsg = TextUtil.GenerateErrorMsg("Je zit niet in de faction waar dit faction " +
                             "blok van is!");
 
